9068222b43e8594ffadcc0bc2bf9628ffa9e5740
Yehor Levchenko <yehorlevchenko@icloud.com>
2021-01-18 20:40:01 +0200

    _clean_data with tuples implemented

--- api_processors/ny_api_processor.py
+++ api_processors/ny_api_processor.py
@@ -9,11 +9,11 @@ class NYAPIProcessor(BaseAPIProcessor):
         super().__init__()
         self.url = "https://api.nytimes.com/svc/news/v3/content/all/all.json"
         self.api_key = settings.NYT_API_KEY
-        self.news_fields = ["slug_name", "section", "subsection", "title",
-                            "abstract", "url", "source", "published_date",
-                            "multimedia", "des_facet", "per_facet",
-                            "org_facet", "geo_facet", "ttl_facet",
-                            "topic_facet", "porg_facet"]
+        self.news_fields = ["title", "abstract", "slug_name", "published_date",
+                            "url", "source", "multimedia"]
+        self.facet_fields = ["des_facet", "per_facet", "org_facet",
+                             "geo_facet", "ttl_facet", "topic_facet",
+                             "porg_facet"]
 
     def _clean_data(self, raw_data):
         """
@@ -31,9 +31,20 @@ class NYAPIProcessor(BaseAPIProcessor):
         if not raw_news:
             raise RuntimeError("No news in received data")
         for item in raw_news:
-            cleaned_data = {k: v for k, v in item.items()
-                            if k in self.news_fields}
-            clean_data.append(cleaned_data)
+            cleaned_data = ["nyt"]
+            cleaned_data.extend([v for k, v in item.items()
+                                 if k in self.news_fields])
+            if not item.get('multimedia'):
+                cleaned_data.extend([None, None])
+            else:
+                normal_media = [m for m in item['multimedia']
+                                if m["format"] == "Normal"]
+                if not normal_media:
+                    cleaned_data.extend([None, None])
+                cleaned_data.extend([normal_media[0]["url"],
+                                     normal_media[0]["copyright"]])
+
+            clean_data.append(tuple(cleaned_data))
 
         return clean_data
 
