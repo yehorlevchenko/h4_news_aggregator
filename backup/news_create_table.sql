cc2d4a0f820abdc0c01eb38368dfce3e71985d1a
liliia <orlova_l.b@outlook.com>
2021-01-21 12:01:41 +0200

    Added UNIQUE to the URL field

--- backup/news_create_table.sql
+++ backup/news_create_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE news (
     id SERIAL,
     source_api VARCHAR(50) NOT NULL,
-    title VARCHAR(512) NOT NULL,
+    title VARCHAR(512)  NOT NULL,
     abstract TEXT,
     slug_name VARCHAR(128),
     published_date TIMESTAMP NOT NULL,
-    url TEXT NOT NULL,
+    url TEXT UNIQUE NOT NULL,
     internal_source VARCHAR(256),
     media_url TEXT,
     media_copyright VARCHAR(256)
