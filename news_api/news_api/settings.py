9ce6250f08e7aa0f6bc283d5d7393c9cab95dd6c
Yehor Levchenko <yehorlevchenko@icloud.com>
2021-02-01 18:54:29 +0200

    Engine hardcoded

--- news_api/news_api/settings.py
+++ news_api/news_api/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'news_api.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": os.environ.get("POSTGRES_ENGINE"),
+        "ENGINE": "django.db.backends.postgresql",
         "NAME": os.environ.get("POSTGRES_DB_NAME"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
