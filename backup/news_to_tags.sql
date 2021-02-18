a0cf2a9b7ff2d1d97510be81428fbc774e355d67
Anastasiia Konvisarova <nastia.konvisarova1@gmail.com>
2021-02-18 11:15:20 +0200

    Too silly but I dont know how to do in any other way

--- backup/news_to_tags.sql
+++ backup/news_to_tags.sql
@@ -1,6 +1,5 @@
 CREATE TABLE news_to_tags (
     id SERIAL PRIMARY KEY,
     news_id INT NOT NULL REFERENCES news (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    tag_id INT NOT NULL REFERENCES tags (id) ON UPDATE CASCADE,
-    CONSTRAINT news_tag_pkey PRIMARY KEY (news_id, tag_id)
+    tag_id INT NOT NULL
 );
