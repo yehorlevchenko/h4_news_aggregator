4907d4dac4b1a597c4c866aeeaa9b18d8bdfafdd
yachtless <andrei.domasov@gmail.com>
2021-02-21 14:00:32 +0200

    added user-friendly news formatting

--- newsbot/newsbot.py
+++ newsbot/newsbot.py
@@ -25,10 +25,12 @@ BASE_URL = 'http://127.0.0.1:8000/api'
 
 bot = telebot.TeleBot(API_TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def start(message):
     _ = requests.get(f'{BASE_URL}/start/', params=message.json['from'])
-    bot.send_message(f'Hello, {message.json["from"].get("username", "User")}')
+    bot.send_message(message.chat.id, f'Hello, {message.json["from"].get("username", "User")}')
+
 
 @bot.message_handler(commands=['source'])
 def send_welcome(message):
@@ -38,13 +40,57 @@ def send_welcome(message):
     panel.add(button_nyt, button_ap)
     bot.send_message(message.chat.id, "Choose source:", reply_markup=panel)
 
+
 @bot.message_handler(commands=['topic'])
 def send_nytimes_settings(message):
     res = requests.get('http://h4newsapi.pythonanywhere.com/tags/')
     bot.send_message(message.chat.id, json.dumps(res.json()[:5]))
 
+# в этом варианте мы вытягиваем руками нужные поля, но они дублируются превьюшкой
+@bot.message_handler(commands=['top1'])
+def send_nytimes_news(message):
+    res = requests.get('http://h4newsapi.pythonanywhere.com/news/')
+    for item in _format_news(res.json()[:1]):
+        bot.send_message(message.chat.id, item, parse_mode='MarkdownV2', disable_web_page_preview=False)
+
+# этот вариант дает в превьюшках то, что мы пытаемся вытянуть руками
+@bot.message_handler(commands=['top2'])
+def send_nytimes_news(message):
+    res = requests.get('http://h4newsapi.pythonanywhere.com/news/')
+    for item in res.json()[:2]:
+        bot.send_message(message.chat.id, _clear_MarkdownV2(item['url']), parse_mode='MarkdownV2')
+
+# в этом варианте мы вытягиваем руками и запрещаем превьюшки, чтобы не дублировать
+@bot.message_handler(commands=['top3'])
+def send_nytimes_news(message):
+    res = requests.get('http://h4newsapi.pythonanywhere.com/news/')
+    for item in _format_news(res.json()[:3]):
+        bot.send_message(message.chat.id, item, parse_mode='MarkdownV2', disable_web_page_preview=True)
+
+# в этом варианте отправляем 15 ссылок одним сообщением
+@bot.message_handler(commands=['top4'])
+def send_nytimes_news(message):
+    res = requests.get('http://h4newsapi.pythonanywhere.com/news/')
+    result = ''
+    for item in _format_news(res.json()[:4]):
+        result = f"{result}\r\n\r\n{item}"
+    bot.send_message(message.chat.id, result, parse_mode='MarkdownV2', disable_web_page_preview=True)
 
 def _format_news(raw_data):
-    pass
+    return [_format_single_entry(news) for news in raw_data]
+
+
+def _clear_MarkdownV2(entry):
+    # _*[]()~>#+-=|{}.!
+    for char in '_*[]()~>#+-=|{}.!':
+        entry = entry.replace(char, f'\{char}')
+    return entry
+
+
+def _format_single_entry(dict):
+    return f"*{_clear_MarkdownV2(dict['title'])}*\r\n" \
+           f"{_clear_MarkdownV2(dict['abstract'])}\r\n" \
+           f"[Подробнее]({_clear_MarkdownV2(dict['url'])})"
+
 
 bot.polling()
